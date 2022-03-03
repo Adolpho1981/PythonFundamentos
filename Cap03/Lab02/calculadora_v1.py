6c640e4c5cf1fa49e8324b1dff5be6a410313eb9
Adolpho1981 <72759131+Adolpho1981@users.noreply.github.com>
2022-03-03 17:45:11 -0300

    Update calculadora_v1.py

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -5,5 +5,21 @@
 # Assista o vídeo com a execução do programa!
 
 print("\n******************* Python Calculator *******************")
-
-
+print('Digite: 1(soma), 2(subtração), 3(produto) ou 4(quociente)')
+operacao = input('Qual operação gostaria de executar?: ')
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+if operacao == "1":
+             soma = num1 + num2
+             print ("A soma desses número é: ", soma)
+elif operacao == "2":
+             diferenca = num1-num2
+             print ('A subtração desses números é: ', diferenca)
+elif operacao == "3":
+             produto = num1*num2
+             print ('O produto desses números é: ', produto)
+elif operacao == "4":
+             quociente = num1/num2
+             print( 'O quociente desses números é: ', quociente)
+else:
+             print('Essa operação não está determinada!')
